1dac0607d4fab8c16d1ee8aa71251fe4e0399712
RichardMarkRoss <cptrichkidprice@gmail.com>
2019-09-12 11:46:21 +0200

    code prep

---
+++ Sequence.java
@@ -0,0 +1,11 @@
+public class Sequance{
+	public static void main(String[] args){
+		ArrayList<String> myList = new ArrayList<>();
+		myList.add("apple");
+		mylist.add("carrot");
+		myList.add(1, "plum");
+		System.out.println(myList);
+	}
+}
+
+// it wass exacly this question but this question is asked in everyones exam
